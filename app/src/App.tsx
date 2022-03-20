c165380262d5d475d4bee136facb365b8a315416
Danny <danny@wednesdaytalent.com>
2022-03-20 16:55:47 -0400

    ah base url

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,17 +2,15 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+import 'antd/dist/antd.css'
+import Home from 'pages/Home/Home.lazy'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-const Test = () => {
-    return <div>Hi I am the Homepage</div>
-}
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Test />}></Route>
+                <Route path="/" element={<Home />}></Route>
             </Routes>
         </BrowserRouter>
     )
