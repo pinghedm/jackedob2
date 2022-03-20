15d679ce8167e13434e9cbe4e4294a518909525d
Danny <danny@wednesdaytalent.com>
2022-03-20 16:44:30 -0400

    component templates

---
+++ app/src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export interface HomeProps {}
+
+const Home = ({}: HomeProps) => {
+    return <div>Home Component</div>
+}
+
+export default Home
---
+++ app/templates/TemplateName.lazy.tsx
@@ -0,0 +1,13 @@
+import React, { lazy, Suspense } from 'react'
+import { TemplateNameProps } from './TemplateName'
+const LazyTemplateName = lazy(() => import('./TemplateName'))
+
+const TemplateName = (
+    props: JSX.IntrinsicAttributes & { children?: React.ReactNode } & TemplateNameProps,
+) => (
+    <Suspense fallback={null}>
+        <LazyTemplateName {...props} />
+    </Suspense>
+)
+
+export default TemplateName
